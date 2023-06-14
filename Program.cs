58313a0fb5f119f0cd9d8170ea7112ae84d39f94
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-06-15 00:21:42 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 
-namespace lab
+namespace la
 {
     public static class Extensions
     {
